b454c384b411e0e6c840ef31448a0d028af9af91
tildaglas <ti7462gl-s@student.lu.se>
2020-12-02 17:26:08 +0100

    added example text and rewrote class

--- Lab4/src/SearchPattern.java
+++ Lab4/src/SearchPattern.java
@@ -7,18 +7,36 @@ import java.util.Scanner;
 public class SearchPattern {
 
 	public static void main(String[] args) {
+		
+		if (args.length != 3) {
+			System.out.println("You need to provide three input arguments.");
+		} else {
+			String pattern = args[1].toLowerCase();
+			File inputFile = new File(args[2]);
 
-		String pattern = args[1];
-		String inputFilePath = args[2];
+			Scanner sc;
+			try {
+				sc = new Scanner(inputFile);
+				StringBuilder sb = new StringBuilder();
+				
+				while (sc.hasNextLine()) {
+					
+					String nextLine = sc.nextLine();
+					if (nextLine.toLowerCase().contains(pattern)) {
+						sb.append(nextLine);						
+					}
+				}
 
-		Scanner sc = new Scanner(inputFilePath);
-		while (sc.hasNext()) {
-			String nextLine = sc.next();
-			if(nextLine.contains(pattern)) {
-				System.out.println(nextLine);
+				System.out.println(sb.toString().length() == 0 ? "No matches found" : sb.toString()); ;
+					
+				
+				sc.close();
+			} catch (FileNotFoundException e) {
+				
 			}
 		}
-		sc.close();
+
+		
 
 	}
 
